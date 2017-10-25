d2a666575248461ec2c7a18c7c2a100490a387d1
sigrivi <sigrivi@uio.no>
2017-10-25 12:28:49 +0200

    finished project3c

--- project3c.py
+++ project3c.py
@@ -22,10 +22,15 @@ def ComapareSolvers(h, t_final = 1.):
 	initial_l_V = P_V[0].angular_momentum() # initial angular momentum P_E
 	l = np.linalg.norm(np.cross(P_E[0].r, P_E[0].v*P_E[0].mass))
 	#print("before", initial_l_E/P_V[0].mass)
+	time0 = time.time()
+	
 
 	x_verlet,y_verlet = solve(P_V, "VelocityVerlet", h, t_final)	# solve with Velocity Verlet
+	time_verlet = time.time()-time0
 
+	time0 = time.time()
 	x_euler,y_euler = solve(P_E, "EulerCromer", h, t_final) 		# solve with Euler Cromer
+	time_euler = time.time()-time0
 
 	diff_en_E = (initial_en_E - P_E[0].total_energy())/initial_en_E 	#relative difference in total energy Euler Cormer
 	diff_en_V = (initial_en_V -  P_V[0].total_energy())/initial_en_V	#relatevi difference in total energy Velocity Verlet
@@ -44,31 +49,37 @@ def ComapareSolvers(h, t_final = 1.):
 #	plt.legend()
 #	plt.show()
 #	plt.savefig('VV_vs_EC2',dpi=225)
-	return(diff_en_E, diff_en_V,diff_l_E, diff_l_V)
+	return(diff_en_E, diff_en_V,diff_l_E, diff_l_V,time_euler, time_verlet)
 
-(a,b,c,d) = ComapareSolvers(1/20, 1.)
+#(a,b,c,d,e,f) = ComapareSolvers(1/20, 1.)
 
 
-def plot_difference(H, t_final =1.): #does not worK!!!
+def plot_difference(H, t_final =1.): #plots the energy difference for h=1/(10H) to h=1/H
 
 	diff_en_E = np.zeros(H)
 	diff_en_V= np.zeros(H)
 	diff_l_E= np.zeros(H)
 	diff_l_V= np.zeros(H)
 	axis = np.zeros(H)
+	time_E = 0
+	time_V = 0
 	for i in range(1,H):
 		h = 1/(10*i)
 		N = np.int(t_final/h)
 		axis[i-1] = h
-		diff_en_E[i-1], diff_en_V[i-1],diff_l_E[i-1], diff_l_V[i-1] = ComapareSolvers(h)
-	plt.plot(np.log10(axis), np.log10(diff_en_E), np.log10(axis), np.log10(diff_en_V))
-	#plt.xlabel("h")
-	#plt.ylabel("relative difference in energy")
+		diff_en_E[i-1], diff_en_V[i-1],diff_l_E[i-1], diff_l_V[i-1], time_E, time_V = ComapareSolvers(h)
+		print(N,"%.5f" %time_E,"%.5f" %time_V, time_V/time_E)
+
+	
+	plt.plot(np.log10(axis), np.log10(diff_en_E), label = "EulerCromer")
+	plt.plot(np.log10(axis), np.log10(diff_en_V), label = "VelocityVerlet")
+	plt.title("Relative difference in total energy as a function of step length h")
+	plt.xlabel("log(h)")
+	plt.ylabel("log(delta E)")
+	plt. legend()
+	plt.savefig('energy_loss',dpi=225)
 	plt.show()
-	#plt.plot(axis, diff_l_E, axis, diff_l_V)
-	#plt.xlabel("h")
-	#plt.ylabel("relative difference in angular_momentum")
-	#plt.show()
+
 
 
 plot_difference(20)
